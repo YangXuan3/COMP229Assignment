bfaad86b9eb3de6a2fc21425e674a0e90346e446
YangXuan3 <yxuan1@my.centennialcollege.ca>
2022-10-23 12:12:57 -0400

    access cloud db

--- server/config/db.js
+++ server/config/db.js
@@ -6,6 +6,7 @@
 */
 module.exports =
  {
- "URI":"mongodb://127.0.0.1:27017/personal_site",
+ //"URI":"mongodb://127.0.0.1:27017/personal_site",
+ "URI":"mongodb+srv://YangXuan:xy123456@mongodbserver.9qpyc25.mongodb.net/?retryWrites=true&w=majority",
  "Secret":"SomeSecret"
 }
